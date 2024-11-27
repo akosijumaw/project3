f364e141b3ce8b6cb781af831d4e9839ca01a018
akosijumaw <42738519+akosijumaw@users.noreply.github.com>
2024-11-27 09:43:11 +0800

    sd

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🎈 Jumar Buladaco Project')
 
 st.write('Hello world!')
+file_path = 'https://raw.githubusercontent.com/akosijumaw/data/refs/heads/main/Processed_GroceryStoreDataSet.csv' 
+data = pd.read_csv(file_path, header=None)
+transactions = data.values.tolist()
