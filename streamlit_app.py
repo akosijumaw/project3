3b367eb51deefd1a4e7f76cdf0a13ed421c019a1
akosijumaw <42738519+akosijumaw@users.noreply.github.com>
2024-11-27 09:43:41 +0800

    dsd

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ st.write('Hello world!')
 file_path = 'https://raw.githubusercontent.com/akosijumaw/data/refs/heads/main/Processed_GroceryStoreDataSet.csv' 
 data = pd.read_csv(file_path, header=None)
 transactions = data.values.tolist()
+
+data
