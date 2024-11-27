ca642d9177489191397e1b911c39d4d7e88f4815
akosijumaw <42738519+akosijumaw@users.noreply.github.com>
2024-11-27 09:59:27 +0800

    dsd

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,5 @@ st.title('🎈 Jumar Buladaco Project')
 
 st.write('Hello world!')
 file_path = 'https://raw.githubusercontent.com/akosijumaw/data/refs/heads/main/GroceryStoreDataSet.csv' 
+df = pd.read_csv(file_path, header=None)
 df.head(10)
