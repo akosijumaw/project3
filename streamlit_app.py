e8102398547a7c04c229f4d167144bd0b27e9a81
akosijumaw <42738519+akosijumaw@users.noreply.github.com>
2024-11-27 09:49:59 +0800

    dfds

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import apriori
 
 st.title('🎈 Jumar Buladaco Project')
 
@@ -8,7 +10,6 @@ file_path = 'https://raw.githubusercontent.com/akosijumaw/data/refs/heads/main/P
 data = pd.read_csv(file_path, header=None)
 transactions = data.values.tolist()
 
-from mlxtend.preprocessing import TransactionEncoder
 
 # Transform the list of transactions into a one-hot-encoded dataframe
 te = TransactionEncoder()
@@ -19,3 +20,10 @@ df_transformed = pd.DataFrame(te_array, columns=te.columns_)
 
 # Display the one-hot-encoded dataframe
 df_transformed
+
+
+# Find frequent itemsets with a minimum support value
+frequent_itemsets = apriori(df_transformed, min_support=0.2, use_colnames=True)
+
+# Display the frequent itemsets
+frequent_itemsets
