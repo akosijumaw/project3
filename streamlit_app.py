820041ccdfeb2bc1dd28c1bae065456331281d4c
akosijumaw <42738519+akosijumaw@users.noreply.github.com>
2024-11-27 09:51:48 +0800

    dsd

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ from mlxtend.frequent_patterns import apriori
 st.title('🎈 Jumar Buladaco Project')
 
 st.write('Hello world!')
-file_path = 'https://raw.githubusercontent.com/akosijumaw/data/refs/heads/main/Processed_GroceryStoreDataSet.csv' 
+file_path = 'https://raw.githubusercontent.com/akosijumaw/data/refs/heads/main/GroceryStoreDataSet.csv' 
 data = pd.read_csv(file_path, header=None)
 transactions = data.values.tolist()
 
