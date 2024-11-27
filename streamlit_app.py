f681d003724d26686d4a3e67fa864e091371670a
akosijumaw <42738519+akosijumaw@users.noreply.github.com>
2024-11-27 09:46:06 +0800

    ds

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,12 @@ file_path = 'https://raw.githubusercontent.com/akosijumaw/data/refs/heads/main/P
 data = pd.read_csv(file_path, header=None)
 transactions = data.values.tolist()
 
-data
+from mlxtend.preprocessing import TransactionEncoder
+
+# Transform the list of transactions into a one-hot-encoded dataframe
+te = TransactionEncoder()
+te_array = te.fit(transactions).transform(transactions)
+df_transformed = pd.DataFrame(te_array, columns=te.columns_)
+
+# Display the one-hot-encoded dataframe
+print(df_transformed.head())
