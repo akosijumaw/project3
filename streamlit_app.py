397d4efc77b1c8fb6abd101b25714d5ae1019500
akosijumaw <42738519+akosijumaw@users.noreply.github.com>
2024-11-27 09:52:29 +0800

    ds

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from mlxtend.preprocessing import TransactionEncoder
 from mlxtend.frequent_patterns import apriori
+from mlxtend.frequent_patterns import association_rules
 
 st.title('🎈 Jumar Buladaco Project')
 
