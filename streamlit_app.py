cf277dc348b342c731cd5ed332ebac44c31476de
akosijumaw <42738519+akosijumaw@users.noreply.github.com>
2024-11-27 09:55:20 +0800

    ddsd

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,29 +8,5 @@ st.title('🎈 Jumar Buladaco Project')
 
 st.write('Hello world!')
 file_path = 'https://raw.githubusercontent.com/akosijumaw/data/refs/heads/main/GroceryStoreDataSet.csv' 
-data = pd.read_csv(file_path, header=None)
-transactions = data.values.tolist()
-
-
-# Transform the list of transactions into a one-hot-encoded dataframe
-te = TransactionEncoder()
-te_array = te.fit(transactions).transform(transactions)
-
-df_transformed = pd.DataFrame(te_array, columns=te.columns_)
-
-
-# Display the one-hot-encoded dataframe
-df_transformed
-
-
-# Find frequent itemsets with a minimum support value
-frequent_itemsets = apriori(df_transformed, min_support=0.2, use_colnames=True)
-
-# Display the frequent itemsets
-frequent_itemsets
-
-# Generate association rules with a minimum confidence value
-rules = association_rules(frequent_itemsets, metric='confidence', min_threshold=0.6)
-
-# Display the rules
-rules
+df = pd.read_csv(file_path, header=None)
+df.shape
