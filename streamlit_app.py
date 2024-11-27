8b7c169674238892dbcf14a157c30ca897f83821
akosijumaw <42738519+akosijumaw@users.noreply.github.com>
2024-11-27 14:23:23 +0800

    ddsad

--- streamlit_app.py
+++ streamlit_app.py
@@ -80,12 +80,7 @@ if uploaded_file:
                             st.write(relevant_rules)
                         else:
                             st.write("No rules found involving the selected item(s).")
-                    #Support vs Confidence
-                    plt.scatter(rules['support'], rules['confidence'], alpha=0.5)
-                    plt.xlabel('support')
-                    plt.ylabel('confidence')
-                    plt.title('Support vs Confidence')
-                    plt.show()
+             
                 else:
                     st.write("No association rules found. Try lowering the minimum confidence.")
             else:
