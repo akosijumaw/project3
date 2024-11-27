7db88dbda51fe53534d9a11d2be6f7c1636db5b6
akosijumaw <42738519+akosijumaw@users.noreply.github.com>
2024-11-27 10:31:31 +0800

    dsd

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,5 +40,5 @@ st.subheader("Frequent Itemsets")
 st.write(frequent_itemsets)
 
 # Generate Association Rules
-rules = association_rules(frequent_itemsets, metric="confidence", min_threshold=min_confidence)
+
 st.subheader("Association Rules")
